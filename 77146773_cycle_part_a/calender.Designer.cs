e4497cd979953def44e490e219c04b5c70409cd1
sachyya <sachyya1@gmail.com>
2016-05-10 19:57:39 +0545

    calender form added to show the data while clicking in a specific date

---
+++ 77146773_cycle_part_a/calender.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace _77146773_cycle_part_a
+{
+    partial class calender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calanderDataGridView = new System.Windows.Forms.DataGridView();
+            this.headerGroupBox = new System.Windows.Forms.GroupBox();
+            this.label_file = new System.Windows.Forms.Label();
+            this.label_interval = new System.Windows.Forms.Label();
+            this.label_smode = new System.Windows.Forms.Label();
+            this.label_date = new System.Windows.Forms.Label();
+            this.label_length = new System.Windows.Forms.Label();
+            this.label_start_time = new System.Windows.Forms.Label();
+            this.label_monitor = new System.Windows.Forms.Label();
+            this.label_version = new System.Windows.Forms.Label();
+            this.summaryGroupBox = new System.Windows.Forms.GroupBox();
+            this.label_pwr = new System.Windows.Forms.Label();
+            this.label_avg = new System.Windows.Forms.Label();
+            this.label_speed = new System.Windows.Forms.Label();
+            this.label_hr = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.calanderDataGridView)).BeginInit();
+            this.headerGroupBox.SuspendLayout();
+            this.summaryGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // calanderDataGridView
+            // 
+            this.calanderDataGridView.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.calanderDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.calanderDataGridView.Location = new System.Drawing.Point(12, 23);
+            this.calanderDataGridView.Name = "calanderDataGridView";
+            this.calanderDataGridView.Size = new System.Drawing.Size(387, 410);
+            this.calanderDataGridView.TabIndex = 0;
+            // 
+            // headerGroupBox
+            // 
+            this.headerGroupBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.headerGroupBox.Controls.Add(this.label_file);
+            this.headerGroupBox.Controls.Add(this.label_interval);
+            this.headerGroupBox.Controls.Add(this.label_smode);
+            this.headerGroupBox.Controls.Add(this.label_date);
+            this.headerGroupBox.Controls.Add(this.label_length);
+            this.headerGroupBox.Controls.Add(this.label_start_time);
+            this.headerGroupBox.Controls.Add(this.label_monitor);
+            this.headerGroupBox.Controls.Add(this.label_version);
+            this.headerGroupBox.Location = new System.Drawing.Point(423, 23);
+            this.headerGroupBox.Name = "headerGroupBox";
+            this.headerGroupBox.Size = new System.Drawing.Size(479, 185);
+            this.headerGroupBox.TabIndex = 1;
+            this.headerGroupBox.TabStop = false;
+            this.headerGroupBox.Text = "Header";
+            // 
+            // label_file
+            // 
+            this.label_file.AutoSize = true;
+            this.label_file.Location = new System.Drawing.Point(269, 135);
+            this.label_file.Name = "label_file";
+            this.label_file.Size = new System.Drawing.Size(23, 13);
+            this.label_file.TabIndex = 7;
+            this.label_file.Text = "File";
+            // 
+            // label_interval
+            // 
+            this.label_interval.AutoSize = true;
+            this.label_interval.Location = new System.Drawing.Point(269, 103);
+            this.label_interval.Name = "label_interval";
+            this.label_interval.Size = new System.Drawing.Size(42, 13);
+            this.label_interval.TabIndex = 6;
+            this.label_interval.Text = "Interval";
+            // 
+            // label_smode
+            // 
+            this.label_smode.AutoSize = true;
+            this.label_smode.Location = new System.Drawing.Point(269, 71);
+            this.label_smode.Name = "label_smode";
+            this.label_smode.Size = new System.Drawing.Size(40, 13);
+            this.label_smode.TabIndex = 5;
+            this.label_smode.Text = "Smode";
+            // 
+            // label_date
+            // 
+            this.label_date.AutoSize = true;
+            this.label_date.Location = new System.Drawing.Point(269, 39);
+            this.label_date.Name = "label_date";
+            this.label_date.Size = new System.Drawing.Size(30, 13);
+            this.label_date.TabIndex = 4;
+            this.label_date.Text = "Date";
+            // 
+            // label_length
+            // 
+            this.label_length.AutoSize = true;
+            this.label_length.Location = new System.Drawing.Point(54, 135);
+            this.label_length.Name = "label_length";
+            this.label_length.Size = new System.Drawing.Size(40, 13);
+            this.label_length.TabIndex = 3;
+            this.label_length.Text = "Length";
+            // 
+            // label_start_time
+            // 
+            this.label_start_time.AutoSize = true;
+            this.label_start_time.Location = new System.Drawing.Point(54, 103);
+            this.label_start_time.Name = "label_start_time";
+            this.label_start_time.Size = new System.Drawing.Size(67, 13);
+            this.label_start_time.TabIndex = 2;
+            this.label_start_time.Text = "Started Time";
+            // 
+            // label_monitor
+            // 
+            this.label_monitor.AutoSize = true;
+            this.label_monitor.Location = new System.Drawing.Point(54, 71);
+            this.label_monitor.Name = "label_monitor";
+            this.label_monitor.Size = new System.Drawing.Size(42, 13);
+            this.label_monitor.TabIndex = 1;
+            this.label_monitor.Text = "Monitor";
+            // 
+            // label_version
+            // 
+            this.label_version.AutoSize = true;
+            this.label_version.Location = new System.Drawing.Point(54, 39);
+            this.label_version.Name = "label_version";
+            this.label_version.Size = new System.Drawing.Size(42, 13);
+            this.label_version.TabIndex = 0;
+            this.label_version.Text = "Version";
+            // 
+            // summaryGroupBox
+            // 
+            this.summaryGroupBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.summaryGroupBox.Controls.Add(this.label_pwr);
+            this.summaryGroupBox.Controls.Add(this.label_avg);
+            this.summaryGroupBox.Controls.Add(this.label_speed);
+            this.summaryGroupBox.Controls.Add(this.label_hr);
+            this.summaryGroupBox.Controls.Add(this.label13);
+            this.summaryGroupBox.Controls.Add(this.label14);
+            this.summaryGroupBox.Controls.Add(this.label15);
+            this.summaryGroupBox.Controls.Add(this.label16);
+            this.summaryGroupBox.Location = new System.Drawing.Point(530, 249);
+            this.summaryGroupBox.Name = "summaryGroupBox";
+            this.summaryGroupBox.Size = new System.Drawing.Size(372, 184);
+            this.summaryGroupBox.TabIndex = 2;
+            this.summaryGroupBox.TabStop = false;
+            this.summaryGroupBox.Text = "Summary";
+            // 
+            // label_pwr
+            // 
+            this.label_pwr.AutoSize = true;
+            this.label_pwr.Location = new System.Drawing.Point(263, 125);
+            this.label_pwr.Name = "label_pwr";
+            this.label_pwr.Size = new System.Drawing.Size(35, 13);
+            this.label_pwr.TabIndex = 15;
+            this.label_pwr.Text = "label9";
+            // 
+            // label_avg
+            // 
+            this.label_avg.AutoSize = true;
+            this.label_avg.Location = new System.Drawing.Point(263, 93);
+            this.label_avg.Name = "label_avg";
+            this.label_avg.Size = new System.Drawing.Size(41, 13);
+            this.label_avg.TabIndex = 14;
+            this.label_avg.Text = "label10";
+            // 
+            // label_speed
+            // 
+            this.label_speed.AutoSize = true;
+            this.label_speed.Location = new System.Drawing.Point(263, 61);
+            this.label_speed.Name = "label_speed";
+            this.label_speed.Size = new System.Drawing.Size(41, 13);
+            this.label_speed.TabIndex = 13;
+            this.label_speed.Text = "label11";
+            // 
+            // label_hr
+            // 
+            this.label_hr.AutoSize = true;
+            this.label_hr.Location = new System.Drawing.Point(263, 29);
+            this.label_hr.Name = "label_hr";
+            this.label_hr.Size = new System.Drawing.Size(41, 13);
+            this.label_hr.TabIndex = 12;
+            this.label_hr.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(48, 125);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(37, 13);
+            this.label13.TabIndex = 11;
+            this.label13.Text = "Power";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(48, 93);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(47, 13);
+            this.label14.TabIndex = 10;
+            this.label14.Text = "Avarage";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(48, 61);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(38, 13);
+            this.label15.TabIndex = 9;
+            this.label15.Text = "Speed";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(48, 29);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(59, 13);
+            this.label16.TabIndex = 8;
+            this.label16.Text = "Heart Rate";
+            // 
+            // calender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 466);
+            this.Controls.Add(this.summaryGroupBox);
+            this.Controls.Add(this.headerGroupBox);
+            this.Controls.Add(this.calanderDataGridView);
+            this.Name = "calender";
+            this.Text = "calender";
+            this.Load += new System.EventHandler(this.calender_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.calanderDataGridView)).EndInit();
+            this.headerGroupBox.ResumeLayout(false);
+            this.headerGroupBox.PerformLayout();
+            this.summaryGroupBox.ResumeLayout(false);
+            this.summaryGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView calanderDataGridView;
+        private System.Windows.Forms.GroupBox headerGroupBox;
+        private System.Windows.Forms.GroupBox summaryGroupBox;
+        public System.Windows.Forms.Label label_file;
+        private System.Windows.Forms.Label label_interval;
+        private System.Windows.Forms.Label label_smode;
+        private System.Windows.Forms.Label label_date;
+        private System.Windows.Forms.Label label_length;
+        private System.Windows.Forms.Label label_start_time;
+        private System.Windows.Forms.Label label_monitor;
+        private System.Windows.Forms.Label label_version;
+        private System.Windows.Forms.Label label_pwr;
+        private System.Windows.Forms.Label label_avg;
+        private System.Windows.Forms.Label label_speed;
+        private System.Windows.Forms.Label label_hr;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
---
+++ 77146773_cycle_part_a/calender.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _77146773_cycle_part_a
+{
+    public partial class calender : Form
+    {
+        public calender()
+        {
+            InitializeComponent();
+        }
+
+        private void calender_Load(object sender, EventArgs e)
+        {
+            int[] arrHR;
+            int[] arSpeed;
+            int[] arrCadance;
+            int[] arrAltitude;
+            int[] arrPower;
+
+
+            string file1 = label_file.Text;
+
+            //Header Information
+            foreach (string line in File.ReadAllLines(file1))
+            {
+                string[] parts = line.Split('=');
+
+
+                if (parts[0].Equals("Version"))
+                {
+                    label_version.Text = parts[1];
+                }
+
+                if (parts[0].Equals("Monitor"))
+                {
+                    label_monitor.Text = parts[1];
+                }
+
+                if (parts[0].Equals("StartTime"))
+                {
+                    label_start_time.Text = parts[1];
+                }
+
+                if (parts[0].Equals("Length"))
+                {
+                    label_length.Text = parts[1];
+                }
+
+                if (parts[0].Equals("SMode"))
+                {
+                    label_smode.Text = parts[1];
+                }
+
+                if (parts[0].Equals("Date"))
+                {
+                    label_date.Text = parts[1];
+                }
+
+                if (parts[0].Equals("Interval"))
+                {
+                    label_interval.Text = parts[1];
+                }
+            }
+
+            // Datagrid view colum header
+        
+            calanderDataGridView.Columns.Add("HR", "HR");
+            calanderDataGridView.Columns.Add("SPD", "SPD");
+            calanderDataGridView.Columns.Add("CAD", "CAD");
+            calanderDataGridView.Columns.Add("ALT", "ALT");
+            calanderDataGridView.Columns.Add("PWR", "PWR");
+
+            string txtData = File.ReadAllText(file1);
+            string[] arrData = txtData.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            int index2 = Array.IndexOf(arrData, "[HRData]");
+            arrHR = new int[arrData.Length - (index2 + 1)];
+            arSpeed = new int[arrData.Length - (index2 + 1)];
+            arrCadance = new int[arrData.Length - (index2 + 1)];
+            arrAltitude = new int[arrData.Length - (index2 + 1)];
+            arrPower = new int[arrData.Length - (index2 + 1)];
+
+            int j = 0;
+            double total = 0;
+            double heart = 0;
+            double powertotal = 0;
+            double alt = 0;
+
+            for (int i = index2 + 1; i < arrData.Length; i++)
+            {
+                string HRData = arrData[i];
+                string[] arrHrdata = Regex.Split(HRData, @"\W+");
+
+                arrHR[j] = Convert.ToInt32(arrHrdata[0]);
+                arSpeed[j] = Convert.ToInt32(arrHrdata[1]);
+                arrCadance[j] = Convert.ToInt32(arrHrdata[2]);
+                arrAltitude[j] = Convert.ToInt32(arrHrdata[3]);
+                arrPower[j] = Convert.ToInt32(arrHrdata[4]);
+
+
+                // for Average speed
+                double speed = Convert.ToDouble(arrHrdata[1]) / 10;
+                double xa = arSpeed[j];
+                total = total + xa;
+
+                //Average heart Rate
+                double hrt = arrHR[j];
+                heart = heart + hrt;
+
+
+                //Average power
+                double pwr = arrPower[j];
+                powertotal = powertotal + pwr;
+
+                double altitude = arrAltitude[j];
+                alt = alt + altitude;
+
+                // Display data in DataGrid view
+                calanderDataGridView.Rows.Add(new object[] { arrHrdata[0], speed.ToString(), arrCadance[j], arrAltitude[j], arrPower[j], (j + 1).ToString() });
+                j++;
+            }
+
+            double km = total / 10;
+
+            //FOr average speed
+            double avgspeed = km / (j);
+            this.label_speed.Text = avgspeed.ToString("#.####");
+
+            //for average Heart Rate
+            double avghr = heart / (j);
+            this.label_hr.Text = avghr.ToString("#.###");
+
+            double avgpwr = powertotal / (j);
+            this.label_pwr.Text = avgpwr.ToString("#.###");
+
+            //for average ALT data
+            double avgalt = alt / (j);
+            this.label_avg.Text = avgalt.ToString("#.###");
+        }
+
+        }
+    }
